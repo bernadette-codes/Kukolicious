63ba83b10a00afb0f21edcc6bd74fb096d72560b
Bernadette <bernadette@englemaninvest.com>
2017-03-18 00:12:01 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,12 +3,12 @@
 
     app.controller('salonPhotosController', function($scope){
         $scope.photoTop = [
-            { alt: 'Salon1', photo: 'img/salon1.jpg'},
-            { alt: 'Salon2', photo: 'img/salon2.jpg'}
+            { alt: 'Salon1', photo: '../img/salon1.jpg'},
+            { alt: 'Salon2', photo: '../img/salon2.jpg'}
         ];
         $scope.photoBottom = [
-            { alt: 'Salon3', photo: 'img/salon3.jpg'},
-            { alt: 'Salon4', photo: 'img/salon4.jpg'}
+            { alt: 'Salon3', photo: '../img/salon3.jpg'},
+            { alt: 'Salon4', photo: '../img/salon4.jpg'}
         ];
     });
 
@@ -79,24 +79,24 @@
 
     app.controller('galleryController', function($scope){
         $scope.galleryR1 = [
-            { alt1: 'leopard nail desig', photo1: 'img/nailDesign15.jpg',
-                alt2: '3 nail design', photo2: 'img/nailDesign13.jpg'},
-            { alt1: 'water transfer', photo1: 'img/nailDesign11.jpg',
-                alt2: 'glitter color nail', photo2: 'img/nailDesign14.jpg'},
-            { alt1: 'feather nail design', photo1: 'img/nailDesign7.jpg',
-                alt2: '3 color nail', photo2: 'img/nailDesign6.jpg'},
-            { alt1: 'symmetric nail design', photo1: 'img/nailDesign5.jpg',
-                alt2: '2 color dotted nail', photo2: 'img/nailDesign3.jpg'}
+            { alt1: 'leopard nail desig', photo1: '../img/nailDesign15.jpg',
+                alt2: '3 nail design', photo2: '../img/nailDesign13.jpg'},
+            { alt1: 'water transfer', photo1: '../img/nailDesign11.jpg',
+                alt2: 'glitter color nail', photo2: '../img/nailDesign14.jpg'},
+            { alt1: 'feather nail design', photo1: '../img/nailDesign7.jpg',
+                alt2: '3 color nail', photo2: '../img/nailDesign6.jpg'},
+            { alt1: 'symmetric nail design', photo1: '../img/nailDesign5.jpg',
+                alt2: '2 color dotted nail', photo2: '../img/nailDesign3.jpg'}
         ];
         $scope.galleryR2 = [
-            { alt1: 'glitter color nail', photo1: 'img/nailDesign10.jpg',
-                alt2: 'glitter nail design', photo2: 'img/nailDesign12.jpg'},
-            { alt1: '3 color nail', photo1: 'img/nailDesign4.jpg',
-                alt2: '3 color nail', photo2: 'img/nailDesign1.jpg'},
-            { alt1: 'symmetric nail design', photo1: 'img/nailDesign8.jpg',
-                alt2: 'symmetric design', photo2: 'img/nailDesign16.jpg'},
-            { alt1: 'heart nail design', photo1: 'img/nailDesign9.jpg',
-                alt2: '2 color nail', photo2: 'img/nailDesign2.jpg'}
+            { alt1: 'glitter color nail', photo1: '../img/nailDesign10.jpg',
+                alt2: 'glitter nail design', photo2: '../img/nailDesign12.jpg'},
+            { alt1: '3 color nail', photo1: '../img/nailDesign4.jpg',
+                alt2: '3 color nail', photo2: '../img/nailDesign1.jpg'},
+            { alt1: 'symmetric nail design', photo1: '../img/nailDesign8.jpg',
+                alt2: 'symmetric design', photo2: '../img/nailDesign16.jpg'},
+            { alt1: 'heart nail design', photo1: '../img/nailDesign9.jpg',
+                alt2: '2 color nail', photo2: '../img/nailDesign2.jpg'}
         ];
     });
 
